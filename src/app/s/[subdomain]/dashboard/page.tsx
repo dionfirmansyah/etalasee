1980d3a97e4a4f7ad830ad6a496197c7e91d08eb
unknown <yoooooooniverse@gmail.com>
2025-09-21 16:16:08 +0800

    fix

--- src/app/s/[subdomain]/dashboard/page.tsx
+++ src/app/s/[subdomain]/dashboard/page.tsx
@@ -1,5 +1,9 @@
-export default async function DashboardTenantPage({ params }: { params: { subdomain: string } }) {
-    const subdomain = params.subdomain;
+export default async function DashboardTenantPage({
+  params,
+}: {
+  params: { subdomain: string };
+}) {
+  const subdomain = params.subdomain;
 
   return (
     <div className="p-6">
