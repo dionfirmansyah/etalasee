65294033e6ec8980e716fb0439c28945e79bbf44
unknown <yoooooooniverse@gmail.com>
2025-09-21 16:51:12 +0800

    ts

--- src/app/s/[subdomain]/dashboard/page.tsx
+++ src/app/s/[subdomain]/dashboard/page.tsx
@@ -1,9 +1,7 @@
-export default async function DashboardTenantPage(
-  { params }: { params: { subdomain: string } }
-) {
+export default function DashboardTenantPage() {
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-bold">DASHBOARD: {params.subdomain}</h1>
+      <h1 className="text-2xl font-bold">DASHBOARD: Tenant</h1>
       <p>loading..</p>
     </div>
   );
--- src/app/s/[subdomain]/page.tsx
+++ src/app/s/[subdomain]/page.tsx
@@ -1,9 +1,7 @@
-export default async function EtalaseTenantPage(
-  { params }: { params: { subdomain: string } }
-) {
+export default async function EtalaseTenantPage() {
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-bold">Etalase: {params.subdomain}</h1>
+      <h1 className="text-2xl font-bold">Etalase: Tenant</h1>
       <p>loading..</p>
     </div>
   );
